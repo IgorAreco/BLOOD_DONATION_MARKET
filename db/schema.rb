92129bbe3785cb11d8e5d64cd38963957ec878ba
Rafael Chun <rafaelchun@gmail.com>
2021-08-25 13:21:38 -0300

    geocoded by address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_154217) do
+ActiveRecord::Schema.define(version: 2021_08_25_161254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_08_24_154217) do
     t.text "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bloods_on_user_id"
   end
 
